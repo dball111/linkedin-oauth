1643a72bf2e39c97c6058807237013185a516b84
Derek Ball <derekball.school@gmail.com>
2015-07-09 13:46:24 -0600

    mvp finished, now starting mongodb stretch

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var db = require('monk')(process.env.MONGOLAB_URI || 'localhost/mydb')
+
 
 router.get('/auth/linkedin', passport.authenticate('linkedin'));
 
@@ -14,5 +16,21 @@ router.get('/auth/linkedin/callback',
     failureRedirect: '/',
     successRedirect: '/'
   }));
+
+
+//mongodb stuff
+// passport.use(new LinkedInStrategy({
+//     clientID: process.env.LINKEDIN_CLIENT_ID,
+//     clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+//     callbackURL: process.env.HOST + "/auth/linkedin/callback",
+//     scope: ['r_emailaddress', 'r_basicprofile'],
+//     state: true
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//     // here, find or create a document in the user collection, and update it's contents
+//     // NOTE: the profile object is unnecessarily big.  Only store the parts you care about here.
+//     done(null, {id: profile.id, displayName: profile.displayName, token: accessToken})
+//   }
+// ));
 
 module.exports = router;
