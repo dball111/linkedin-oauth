85f3831c555e600a604792a151e9998a3d05ae79
Derek Ball <derekball.school@gmail.com>
2015-07-09 13:48:10 -0600

    fixing heroku

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var db = require('monk')(process.env.MONGOLAB_URI || 'localhost/mydb')
+// var db = require('monk')(process.env.MONGOLAB_URI || 'localhost/mydb')
 
 
 router.get('/auth/linkedin', passport.authenticate('linkedin'));
@@ -19,18 +19,6 @@ router.get('/auth/linkedin/callback',
 
 
 //mongodb stuff
-// passport.use(new LinkedInStrategy({
-//     clientID: process.env.LINKEDIN_CLIENT_ID,
-//     clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-//     callbackURL: process.env.HOST + "/auth/linkedin/callback",
-//     scope: ['r_emailaddress', 'r_basicprofile'],
-//     state: true
-//   },
-//   function(accessToken, refreshToken, profile, done) {
-//     // here, find or create a document in the user collection, and update it's contents
-//     // NOTE: the profile object is unnecessarily big.  Only store the parts you care about here.
-//     done(null, {id: profile.id, displayName: profile.displayName, token: accessToken})
-//   }
-// ));
+
 
 module.exports = router;
